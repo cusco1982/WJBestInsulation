7cf6449e52bc9e8cbdb471527e40d73df64be515
Julian <cusco2891@gmail.com>
2019-11-11 19:49:01 -0500

    htmlroutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,10 +8,26 @@ var path = require('path');
 module.exports = function(app) {
 
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/home.html'));
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
 
-    app.get('/survey', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/survey.html'));
+    app.get('/services', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/services.html'));
+    });
+
+    app.get('/licensing', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/licensing.html'));
+    });
+
+    app.get('/projects', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/projects.html'));
+    });
+
+    app.get('/company', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/company.html'));
+    });
+
+    app.get('/contact', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/contact.html'));
     });
 };
