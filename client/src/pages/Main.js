b816216609a3f5a3c80fe0a3c5ddc46f7dd7f020
Julian <cusco2891@gmail.com>
2019-11-14 18:44:24 -0500

    working mainpage css/videoedited

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CardMain from "../components/CardMain";
 import video from "../images/video.mp4";
-import image1 from '../images/1.jpg'; 
+import image1 from '../images/1.jpg';
 import image2 from '../images/2.jpg';
 import image3 from '../images/3.jpg';
 import image4 from '../images/4.jpg';
@@ -12,13 +12,13 @@ const API = {
     return ([{
       image: image1,
       title: "New Appartment"
-    },{
+    }, {
       image: image2,
       title: "New Appartment"
-    },{
+    }, {
       image: image3,
       title: "New Appartment"
-    },{
+    }, {
       image: image4,
       title: "New Appartment"
     }])
@@ -31,7 +31,7 @@ const API = {
 class Main extends Component {
   state = {
     appartments: [],
-    
+
   };
 
   open = () => {
@@ -63,33 +63,48 @@ class Main extends Component {
   render() {
     return (
       <div>
-        
-      {/* <div className="container-fluid"> */}
-        {/* <video autoPlay muted loop id="myVideo"> */}
-          {/* <source src={video} type="video/mp4" /> */}
-        {/* </video> */}
 
-        {/* <div className="row animated fadeInDown delay-1s"> */}
-          {/* <div className="col-md-12 search"> */}
-            {/* <h1 className="htitle">Welcome to Advanced Property Management</h1> */}
-          {/* </div> */}
-        {/* </div> */}
-        {/* <div className="content"> */}
-              {/* <h3 className="hed3">Featured properties</h3> */}
-         
-          {/* <div className="row justify-content-center"> */}
-              
-              {/* {this.state.appartments.map(appartment => { */}
-                {/* return <CardMain image={appartment.image} title={appartment.title} open={this.open} /> */}
-                
-              {/* })} */}
-          {/* </div> */}
-        {/* </div> */}
-      {/* </div> */}
-      
+
+        <div className="row1">
+
+          <div className="container-fluid">
+            <video autoPlay muted loop id="myVideo">
+              <source src={video} type="video/mp4" />
+            </video>
+
+            <div className="row animated fadeInDown delay-1s">
+              <div className="col-md-12 search">
+                <h1 className="htitle">Welcome to Advanced Property Management</h1>
+              </div>
+            </div>
+            <div className="content">
+              <h3 className="hed3">Featured properties</h3>
+
+              <div className="row justify-content-center">
+
+                {this.state.appartments.map(appartment => {
+                  return <CardMain image={appartment.image} title={appartment.title} open={this.open} />
+
+                })}
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+
+
+        <div className="row2">
+
+          {this.state.appartments.map(appartment => {
+            return <CardMain image={appartment.image} title={appartment.title} open={this.open} />
+
+          })}
+
+        </div>
+
+
       </div>
-
-
 
     );
   }
