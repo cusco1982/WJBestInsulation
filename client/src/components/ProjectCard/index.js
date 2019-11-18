2f1debdd50f43a2d8715c0634477675557981c67
Julian <cusco2891@gmail.com>
2019-11-18 00:32:52 -0500

    nav height shortened

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -4,9 +4,17 @@ import React from "react";
 function ProjectCard(props) {
     return (
         <div className="col-sm-4">
-            <div className="animated fadeInUp delay-1s" style={{cursor:"pointer"}} style={{heigh:"200px"}}>
-                <img src={props.image} className="card-img-top" alt="New Property" />
-                
+            <div className="animated fadeInUp delay-1s" style={{cursor:"pointer"}}>
+                <img style={{height:"600px"}} src={props.image} className="card-img-top" alt="WJ Best Insulation" />
+
+                {/* <div className="card-body">
+                    <h3 className="tool-tip inner">{props.address}</h3>
+                    <h3 className="card-text">{props.city}</h3><br />
+                    <div className="d-flex justify-content-center">
+                        <h4 className="card-text">{props.price} {props.other}</h4>
+                    </div>
+                </div> */}
+
                 {/* <button type="button" className="btn btn-primary btn-lg btn-block" onClick={() => props.open(props.id)}>Request Info</button> */}
             </div>
         </div>
