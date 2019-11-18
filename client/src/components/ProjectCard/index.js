4fcafe55de9fd94ec646d849bcac1984cd3e76e5
Julian <cusco2891@gmail.com>
2019-11-18 00:28:17 -0500

    cardbody deleted

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -4,16 +4,9 @@ import React from "react";
 function ProjectCard(props) {
     return (
         <div className="col-sm-4">
-            <div className="animated fadeInUp delay-1s">
+            <div className="animated fadeInUp delay-1s" style={{cursor:"pointer"}} style={{heigh:"200px"}}>
                 <img src={props.image} className="card-img-top" alt="New Property" />
-                <div className="card-body">
-                    <h3 className="card-text">{props.address}</h3>
-                    <h3 className="card-text">{props.city}</h3><br />
-                    <div className="d-flex justify-content-center">
-                        <h4 className="card-text">{props.price} {props.other}</h4>
-                    </div>
-
-                </div>
+                
                 {/* <button type="button" className="btn btn-primary btn-lg btn-block" onClick={() => props.open(props.id)}>Request Info</button> */}
             </div>
         </div>
