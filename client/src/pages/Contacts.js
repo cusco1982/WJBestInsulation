dc272a9065bd491c85125604aeee81e5a4a88290
Julian <cusco2891@gmail.com>
2020-03-04 00:00:45 -0500

    spacing

--- client/src/pages/Contacts.js
+++ client/src/pages/Contacts.js
@@ -40,8 +40,8 @@ class Contacts extends Component {
               <br />
               <br />
 
-              <h1 style={{float: "right"}}>HAVE A PROJECT IN MIND?</h1><br />
-              <h4>Questions about an upcoming project or need a quote? We’d be glad to help.</h4><br />
+              <h1 style={{float: "center"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;HAVE A PROJECT IN MIND?</h1><br />
+              <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Questions about an upcoming project or need a quote? &nbsp;&nbsp;We’d be glad to help.</h4><br />
               <Contact onChange={this.onChange} onSubmit={this.onSubmit} />
             </div>
           </div>
