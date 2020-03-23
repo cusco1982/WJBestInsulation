1797baaf7804307f70ed0f4dcb53a99624fda5c5
Julian <cusco2891@gmail.com>
2020-03-23 02:57:34 -0400

    contact consolelog

--- client/src/pages/Contacts.js
+++ client/src/pages/Contacts.js
@@ -16,35 +16,39 @@ class Contacts extends Component {
 
 
   onSubmit = () => {
-    const fname = this.state.fname;
-    const lname = this.state.lname;
-    const email = this.state.email;
-    const phone = this.state.phone;
-    const message = this.state.message;
-
-
-    API.contact({ fname: fname, lname: lname, email: email, phone: phone, message: message })
+    console.log(this.state);
+    // const fname = this.state.fname;
+    // const lname = this.state.lname;
+    // const email = this.state.email;
+    // const phone = this.state.phone;
+    // const message = this.state.message;
+    // API.contact({ fname: fname, lname: lname, email: email, phone: phone, message: message })
   }
   render() {
     return (
 
       <div>
-        <div className="container col-md-7" style={{ marginTop: "30px", float:"left" }}>
+        <div className="container col-md-7" style={{ marginTop: "30px", float: "left" }}>
           <div className="jumbotron form animated fadeInUp slow">
             <div className="contact-left">
               <br />
               <br />
               <br />
               <br />
-              <h1 style={{float: "center"}}>&nbsp;&nbsp;&nbsp;&nbsp;HAVE A PROJECT IN MIND?</h1><br />
-              <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Questions about an upcoming project or need a quote? &nbsp;&nbsp;We’d be glad to help!</h4><br />
+              <h1 style={{ float: "center" }}>
+                &nbsp;&nbsp;&nbsp;&nbsp;HAVE A PROJECT IN MIND?
+              </h1>
+              <br />
+              <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                Questions about an upcoming project or need a quote?
+                &nbsp;&nbsp;
+                We’d be glad to help!
+              </h4>
+              <br />
               <Contact onChange={this.onChange} onSubmit={this.onSubmit} />
             </div>
           </div>
         </div>
-
-
-
       </div>
 
     )
