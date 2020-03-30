6c58e9d765b14107b20b27d7da5f2d11f26aa06f
Julian <cusco2891@gmail.com>
2020-03-30 01:25:29 -0400

    navlogo added//needs sizing

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../images/logoo.png";
+import logo from "../../images/navlogo.png";
 import "./style.css";
 
 
