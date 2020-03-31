58cd2adceb0837232be7b2d2939d6738daebbd46
Julian <cusco2891@gmail.com>
2020-03-30 23:32:47 -0400

    clean

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,21 +8,20 @@ import "./style.css";
 function Footer() {
     return (
         <div class="footer-container" id="footer-container">
+
             <div>
 
 
-                <div class="row1">
-
+                <div>
 
                     <div>
                         <p>© 2019 WJ Best Insulation, LLC <a href="">Privacy Policy</a> | <a href="">Legal</a></p>
                     </div>
 
-
-
-
                 </div>
+
             </div>
+
         </div>
     );
 }
