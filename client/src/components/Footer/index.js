5472b70a5a9ecb42f0e15cd5f77f31434c55afb4
Julian <cusco2891@gmail.com>
2020-03-30 22:56:15 -0400

    footer clean

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,30 +8,17 @@ import "./style.css";
 function Footer() {
     return (
         <div class="footer-container" id="footer-container">
-            <div class="et_pb_section et_pb_section_2 et_pb_with_background et_section_regular">
+            <div>
 
 
-                <div class="et_pb_row et_pb_row_1">
-                    <div class="et_pb_column et_pb_column_4_4 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough et-last-child">
+                <div class="row1">
 
 
-                        <div class="et_pb_module et_pb_text et_pb_text_1 et_pb_bg_layout_light  et_pb_text_align_center">
-
-
-                            <div class="et_pb_text_inner">
-                                <p>© 2019 WJ Best Insulation, LLC <a href="http://nyinsulates.com/privacy-policy/">Privacy Policy</a> | <a href="http://nyinsulates.com/legal/">Legal</a></p>
-                            </div>
-
-                        </div>
-
-
-                        <ul class="et_pb_module et_pb_social_media_follow et_pb_social_media_follow_0 clearfix et_pb_bg_layout_light  et_pb_text_align_center">
-
-                            <li class="et_pb_social_media_follow_network_0 et_pb_social_icon et_pb_social_network_link  et-social-linkedin et_pb_social_media_follow_network_0"><a href="https://www.linkedin.com/company/new-york-insulation-inc" class="icon et_pb_with_border" title="Follow on LinkedIn" target="_blank"><span class="et_pb_social_media_follow_network_name" aria-hidden="true">Follow</span></a></li>
-
-                        </ul>
-
+                    <div>
+                        <p>© 2019 WJ Best Insulation, LLC <a href="">Privacy Policy</a> | <a href="">Legal</a></p>
                     </div>
+
+
 
 
                 </div>
