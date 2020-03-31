b5137b457cf87cfd0837fae4dbfc3f0253790c5b
Julian <cusco2891@gmail.com>
2020-03-30 22:14:22 -0400

    format

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -9,52 +9,52 @@ import "./style.css";
 
 function Nav() {
   return (
-    
+
     <div className="container-fluid contnav" id="contnav">
-    <div className="container">
-<nav className="navbar navbar-expand-lg navbar-light navigator" >
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light navigator" >
 
-<Link className="navbar-brand navbrand animated slideInLeft slow" to="/">
-  <img id="logo" className="d-inline-block img-fluid" alt="Logo" src={logo} style={{height:"20px"}}/>
-</Link>
+          <Link className="navbar-brand navbrand animated slideInLeft slow" to="/">
+            <img id="logo" className="d-inline-block img-fluid" alt="Logo" src={logo} style={{ height: "20px" }} />
+          </Link>
 
-<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-  <span className="navbar-toggler-icon"></span>
-</button>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-<div className="collapse navbar-collapse" id="navbarSupportedContent">
-  <ul className="navbar-nav ml-auto mr-auto animated slideInRight slow">
-    <li className="nav-item active">
-      <Link className="nav-link" to="/">HOME</Link>
-    </li>
-    <li className="nav-item">
-      <Link className="nav-link" to="/properties">PROPERTIES</Link>
-    </li>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto mr-auto animated slideInRight slow">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/">HOME</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/properties">PROPERTIES</Link>
+              </li>
 
-    {/* <li className="nav-item">
+              {/* <li className="nav-item">
       <Link className="nav-link" to="/tenants">TENANTS</Link>
     </li> */}
 
-    <li className="nav-item">
-      <Link className="nav-link" to="/contact">Contact Us</Link>
-    </li>
-    
-    <li className="nav-item">
-        <Link className="nav-link" to="/tenants">LOGIN</Link>
-    </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">Contact Us</Link>
+              </li>
 
-    <li className="nav-item">
-        <Link className="nav-link" to="/projects">PROJECTS</Link>
-    </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/tenants">LOGIN</Link>
+              </li>
 
-  </ul>
-  </div>
-  
-</nav>
-</div>
-</div>
-);
+              <li className="nav-item">
+                <Link className="nav-link" to="/projects">PROJECTS</Link>
+              </li>
+
+            </ul>
+          </div>
+
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
