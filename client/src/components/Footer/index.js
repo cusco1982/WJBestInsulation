e6641543c12fccdbde8ce5e440d9eeb49bff2041
Julian <cusco2891@gmail.com>
2020-03-31 02:43:24 -0400

    footer socials templat

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -12,9 +12,34 @@ function Footer() {
             <div className="row">
 
                 <div className='left col-md-6'>
-                    <h3>GET IN TOUCH:</h3>
-                    <p>Tel: 732-423-9671</p>
-                    <p>Email: WJBestInsulation@gmail.com</p>
+
+
+                    <h3>GET IN TOUCH:</h3><br />
+
+                    <div className="row">
+                        <div className="left col-md-6">
+                            <p>Tel: 732-423-9671</p>
+                            <p>Fax: 732-423-9671</p>
+                            {/* <p>Email: WJBestInsulation@gmail.com</p> */}
+                        </div>
+                        <div className="right col-md-6">
+                            <p>2 Nora Rd</p>
+                            <p>Edison, NJ 08837</p>
+                        </div>
+                    </div>
+
+                    <div className="row2">
+
+                    <a href="">facebook</a>
+                    <a href="">linkedIn</a>
+                    <a href="">Instagram</a>
+                    <a href="">GoogleLink</a>
+
+                    
+
+                    </div>
+
+
                 </div>
 
 
@@ -22,37 +47,37 @@ function Footer() {
 
                     <div className="row">
 
-                    <form
-                // onSubmit={this.submitForm}
-                action="https://formspree.io/xjvolkon"
-                method="POST"
-              // enctype="multipart/form-data"
-              >
+                        <form
+                            // onSubmit={this.submitForm}
+                            action="https://formspree.io/xjvolkon"
+                            method="POST"
+                        // enctype="multipart/form-data"
+                        >
 
-                <label>Name:</label>
-                {/* &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>Company:</label> */}
-                <br />
-                <input class='form-control' type="text" name="name" />
-                {/* <input type="text" name="company" />          */}
-                <br />
-                <label>Email:</label>
-                {/* &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>Phone:</label> */}
-                <br />
-                <input class='form-control' type="email" name="email" />
-                {/* <input type="phone" name="phone" /> */}
-                <br />
-                <label>Message:</label>
-                <br />
-                <textarea class='form-control' type="textarea" name="message" />
-                <br />
-                <br />
-                {/* <br/>
+                            <label>Name:</label>
+                            {/* &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>Company:</label> */}
+                            <br />
+                            <input class='form-control' type="text" name="name" />
+                            {/* <input type="text" name="company" />          */}
+                            <br />
+                            <label>Email:</label>
+                            {/* &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <label>Phone:</label> */}
+                            <br />
+                            <input class='form-control' type="email" name="email" />
+                            {/* <input type="phone" name="phone" /> */}
+                            <br />
+                            <label>Message:</label>
+                            <br />
+                            <textarea class='form-control' type="textarea" name="message" />
+                            <br />
+                            <br />
+                            {/* <br/>
                 <label>Your file:</label>
                 <input type="file" name="upload" /> */}
-                {/* {status === "SUCCESS" ? <p>Thanks!</p> :  */}
-                <button>Submit</button>
-                {/* {status === "ERROR" && <p>Ooops! There was an error.</p>} */}
-              </form>
+                            {/* {status === "SUCCESS" ? <p>Thanks!</p> :  */}
+                            <button>Submit</button>
+                            {/* {status === "ERROR" && <p>Ooops! There was an error.</p>} */}
+                        </form>
 
 
 
