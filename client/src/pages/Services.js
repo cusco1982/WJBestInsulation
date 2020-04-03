009fe75cb66d7e659192e13e574016eda2294a09
Julian <cusco2891@gmail.com>
2020-04-02 23:58:16 -0400

    slider temp

--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -71,7 +71,7 @@ class Services extends Component {
             <div>
 
 
-                    <img id="logo" className="d-inline-block img-fluid slideInLeft" alt="Logo" src={domino} />
+                    <img id="logo" alt="Logo" src={domino} style={{width:'100%', height:'630px'}} />
 
 
                 <div className="row services">
@@ -85,9 +85,42 @@ class Services extends Component {
                     </ul>
                 </div>
 
+
                 <div className="row2">
-                    images going by area located on bottom middle of screen
-                </div>
+<!-- Slideshow container -->
+<div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img src="img1.jpg" style="width:100%">
+    <div class="text">Caption Text</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="img2.jpg" style="width:100%">
+    <div class="text">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="img3.jpg" style="width:100%">
+    <div class="text">Caption Three</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>                </div>
 
             </div>
 
